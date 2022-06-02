8dd126165aab3b8b8e2f9a7b6ee982b7384e61cf
nata <104881649+natamz@users.noreply.github.com>
2022-06-02 22:27:07 +0900

    add roulette rotation

--- components/Wheel.vue
+++ components/Wheel.vue
@@ -1,5 +1,10 @@
 <template>
-  <canvas id="canvas" width="300" height="300"></canvas>
+  <canvas ref="canvas" id="canvas" width="300" height="300"></canvas>
+
+  <div>
+    {{ result }}
+    <v-btn @click="start">start</v-btn>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,38 +12,67 @@
 const CENTER_X: number = 150;
 const CENTER_Y: number = 150;
 
+export interface WheelItem {
+  id: number;
+  value: string;
+  color: string;
+}
+
 // radianに変換
 function ToRadian(deg: number): number {
-  return ((360 - deg) * Math.PI) / 180;
+  return ((deg % 360) * Math.PI) / 180;
 }
 
 export default {
+  data(): { result: string } {
+    return {
+      result: "",
+    };
+  },
   props: {
-    items: { type: Array, default: [] },
+    items: { type: Object as () => WheelItem, default: [] },
   },
   methods: {
-    draw() {
-      let start_deg: number = 0;
+    draw(start_deg: number = 0) {
+      this.ctx.clearRect(0, 0, 300, 300);
+
       let deg: number = 360 / this.items.length;
 
-      this.items.forEach((element: { color: string }) => {
-        this.drawPart(start_deg, start_deg + deg, element.color);
+      this.items.forEach((element: WheelItem) => {
+        this.drawPart(start_deg, start_deg + deg, element);
         start_deg += deg;
       });
     },
 
-    drawPart(start_deg: number, end_deg: number, color: string) {
-      let start_rad: number = ToRadian(start_deg);
-      let end_rad: number = ToRadian(end_deg);
+    drawPart(start_deg: number, end_deg: number, element: WheelItem) {
+      const start_rad: number = ToRadian(start_deg);
+      const end_rad: number = ToRadian(end_deg);
+
       this.ctx.beginPath();
       this.ctx.moveTo(CENTER_X, CENTER_Y);
-      this.ctx.fillStyle = color;
-      this.ctx.arc(CENTER_X, CENTER_Y, 100, start_rad, end_rad, true);
+      this.ctx.fillStyle = element.color;
+      this.ctx.arc(CENTER_X, CENTER_Y, 100, start_rad, end_rad);
       this.ctx.fill();
+    start() {
+      const num = Math.random() * 360;
+      const i = Math.floor(num / (360 / this.items.length));
+      this.result = this.items[i].value;
+      let speed: number = 10000;
+
+      let timer = setInterval(() => {
+        speed *= 0.985;
+
+        this.draw(-num - speed - 90);
+
+        if (speed < 0.1) {
+          // 停止
+          clearInterval(timer);
+        }
+      }, 10);
     },
   },
   mounted() {
-    this.ctx = this.$el.getContext("2d");
+    this.ctx = this.$refs.canvas.getContext("2d");
     this.draw();
   },
 };

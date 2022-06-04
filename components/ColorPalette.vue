85f2f880caa9044b131dbb44995ecc6874fe64c4
nata <104881649+natamz@users.noreply.github.com>
2022-06-04 21:47:41 +0900

    add colorPalette component

---
+++ components/ColorPalette.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-dialog max-width="800px" v-model="dialog">
+      <v-card>
+        <v-card-text>
+          <v-row>
+            <v-col v-for="color in colors">
+              <v-btn
+                @click="colorSubmit(color)"
+                :style="'background-color:' + color"
+              ></v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import { colors } from "@/consts/colors";
+
+export default {
+  data(): {
+    id: Number;
+    dialog: boolean;
+    colors: ReadonlyArray<string>;
+  } {
+    return {
+      id: 0,
+      dialog: false,
+      colors: colors,
+    };
+  },
+  methods: {
+    showDialog(id: number) {
+      this.id = id;
+      this.dialog = true;
+    },
+    colorSubmit(color: string) {
+      this.$emit("colorSubmit", { id: this.id, color: color });
+      this.dialog = false;
+    },
+  },
+};
+</script>
---
+++ consts/colors.ts
@@ -0,0 +1,16 @@
+export const colors: ReadonlyArray<string> = [
+  "#ff4500",
+  "#0000cd",
+  "#66cdaa",
+  "#ff8c00",
+  "#00ff7f",
+  "#ffefd5",
+  "#dcdcdc",
+  "#006400",
+  "#8a2be2",
+  "#808000",
+  "#ffd700",
+  "#f0e68c",
+  "#2f4f4f",
+  "#f0f8ff",
+];

c54fe0f1208976669192187e1d54bd3fe6327b5a
nata <104881649+natamz@users.noreply.github.com>
2022-06-05 17:26:45 +0900

    remove pages directory

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar dense elevation="1" outlined>
+    <v-app-bar elevation="1">
       <v-app-bar-title>roulette</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
-      <nuxt-page></nuxt-page>
+      <v-container>
+        <roulette></roulette>
+      </v-container>
     </v-main>
   </v-app>
 </template>
--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <v-container>
-    <roulette></roulette>
-  </v-container>
-</template>

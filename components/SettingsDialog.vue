fcb59bcef860ae7527f97290621772611f691e99
nata <104881649+natamz@users.noreply.github.com>
2022-06-23 08:55:00 +0900

    #31 add rotation time (moment)

--- components/SettingsDialog.vue
+++ components/SettingsDialog.vue
@@ -21,10 +21,15 @@
         <v-divider></v-divider>
         <v-list>
           <v-list-item>
-            回転時間
-            <v-btn @click="rotationTime(1000)">短い</v-btn>
-            <v-btn @click="rotationTime(8000)">普通</v-btn>
-            <v-btn @click="rotationTime(10000)">長い</v-btn>
+            <v-list-item-title class="mr-3"> 回転時間 </v-list-item-title>
+            <v-list-item-action v-for="item in rotationTimes">
+              <v-btn
+                @click="setRotationTime(item.value)"
+                :color="item.value == rotationTimeValue ? 'primary' : ''"
+                class="ma-1"
+                >{{ item.name }}
+              </v-btn>
+            </v-list-item-action>
           </v-list-item>
         </v-list>
       </v-card>
@@ -33,28 +38,41 @@
 </template>
 
 <script lang="ts">
+class rotationTime {
+  name: string;
+  value: number;
+}
+const rotationTimes: rotationTime[] = [
+  { name: "一瞬", value: 0 },
+  { name: "短い", value: 1000 },
+  { name: "普通", value: 8000 },
+  { name: "長い", value: 10000 },
+];
+
 export default {
   data(): {
     dialog: boolean;
+    rotationTimes: rotationTime[];
+    rotationTimeValue: number;
   } {
     return {
       dialog: false,
+      rotationTimes: rotationTimes,
+      rotationTimeValue: 0,
     };
   },
   methods: {
     showDialog() {
       this.dialog = true;
     },
-    rotationTime(num: number) {
-      this.$params.rotationTime = num;
+    setRotationTime(num: number) {
+      this.$params.rotationTime = this.rotationTimeValue = num;
       localStorage.setItem("rotationTime", `${num}`);
     },
   },
   mounted() {
-    const rotationTime = localStorage.getItem("rotationTime");
-    if (rotationTime) {
-      this.$params.rotationTime = rotationTime;
-    }
+    this.$params.rotationTime = this.rotationTimeValue =
+      localStorage.getItem("rotationTime") ?? 8000;
   },
 };
 </script>

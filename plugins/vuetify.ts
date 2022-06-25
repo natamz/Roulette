47a492e9ab497cd995bc14c2df5351b3394add6f
nata <104881649+natamz@users.noreply.github.com>
2022-06-25 19:32:14 +0900

    #34 fix cannot display mdi

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,8 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(

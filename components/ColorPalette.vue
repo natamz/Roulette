40a27dc356c6b820e6bec1784e8840b979fd4bdd
nata <104881649+natamz@users.noreply.github.com>
2022-06-30 08:24:33 +0900

    #41 add colors

--- components/ColorPalette.vue
+++ components/ColorPalette.vue
@@ -2,10 +2,17 @@
   <div>
     <v-dialog max-width="800px" v-model="dialog">
       <v-card>
+        <v-card-actions>
+          <v-btn icon @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-actions>
+
         <v-card-text>
-          <v-row>
-            <v-col v-for="color in colors">
+          <v-row v-for="colors in colorGroups" class="mt-1 mb-5">
+            <v-col v-for="color in colors" cols="3" sm="2">
               <v-btn
+                block
                 @click="colorSubmit(color)"
                 :style="'background-color:' + color"
               ></v-btn>
@@ -24,12 +31,12 @@ export default {
   data(): {
     id: Number;
     dialog: boolean;
-    colors: ReadonlyArray<string>;
+    colorGroups: ReadonlyArray<ReadonlyArray<string>>;
   } {
     return {
       id: 0,
       dialog: false,
-      colors: colors,
+      colorGroups: colors,
     };
   },
   methods: {

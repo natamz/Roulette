02e0c6273b55ceffe1aa74c2ba1a4977367c7c37
nata <104881649+natamz@users.noreply.github.com>
2022-07-06 21:51:01 +0900

    remove unnecessary code

--- plugins/params.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "@nuxt/types";
-import { Inject } from "@nuxt/types/app";
-
-export default function (context: Context, inject: Inject): void {
-  const params = {
-    rotationTime: 8000,
-  };
-  inject("params", params);
-}

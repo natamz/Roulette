4a94789dab66b5bcb542b1be969f9a11eafc4999
nata <104881649+natamz@users.noreply.github.com>
2022-08-03 08:46:31 +0900

    fix path
    add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import { defineNuxtConfig } from "nuxt";
 
+const baseURL = process.env.baseURL;
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["vuetify/lib/styles/main.sass", "@/assets/styles/theme.scss"],
@@ -13,6 +15,34 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    baseURL: process.env.baseURL,
+    baseURL: baseURL,
+    head: {
+      title: "ルーレット",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, maximum-scale=1",
+        },
+        { name: "description", content: "roulette web application" },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: `${baseURL}/favicons/favicon.ico`,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: `${baseURL}/favicons/icon-192x192.png`,
+        },
+        {
+          rel: "apple-touch-icon",
+          type: "image/png",
+          href: `${baseURL}/favicons/apple-touch-icon-180x180.png`,
+        },
+      ],
+    },
   },
 });
